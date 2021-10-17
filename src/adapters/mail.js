435405b39efba80f1fd87b9810adfc40998c6a2d
PalomaReb <paloma.rebolleda@gmail.com>
2021-10-17 22:47:25 +0200

    psw

--- src/adapters/mail.js
+++ src/adapters/mail.js
@@ -8,7 +8,7 @@ export const sendMail = (to, subject, content) => {
         secure: true,
         auth: {
             user: 'code.or.die2021@gmail.com',
-            pass: 'kinwhqugrlbdquf',
+            pass: 'acoggsykrwhvikbo',//'kinwhqugrlbdquf', 
         },
         tls: {
             rejectUnauthorized: false
